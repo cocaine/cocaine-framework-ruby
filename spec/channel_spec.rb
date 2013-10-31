c7a5dc3e79f550ddf7b884d6b54b70a0c23cf85a
Evgeny Safronov <division494@gmail.com>
2013-10-31 16:20:00 +0400

    Test added to check errorback immediately invocation.
     * fixed another test.

--- spec/channel_spec.rb
+++ spec/channel_spec.rb
@@ -4,8 +4,24 @@ require_relative '../lib/channel'
 
 describe Cocaine::Channel do
   it 'should invoke callback immediately if it has pending data' do
+    done = false
     channel = Cocaine::Channel.new
     channel.trigger 'actual'
-    channel.callback { |data| expect(data).to eq('actual') }
+    channel.callback { |data|
+      expect(data).to eq('actual')
+      done = true
+    }
+    done.should == true
+  end
+
+  it 'should invoke errback immediately if it has pending errors' do
+    done = false
+    channel = Cocaine::Channel.new
+    channel.error 'actual'
+    channel.errback { |data|
+      expect(data).to eq('actual')
+      done = true
+    }
+    done.should == true
   end
 end
