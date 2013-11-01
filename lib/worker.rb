0efe77e894bbd34d8ef37bfc471a7a7a23bc9cd8
Evgeny Safronov <division494@gmail.com>
2013-11-01 16:43:11 +0400

    Worker stub has been added.

---
+++ lib/worker.rb
@@ -0,0 +1,2 @@
+class Cocaine::Worker
+end
---
+++ spec/worker_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+require_relative '../lib/worker'
+
+describe Cocaine::Worker do
+
+  it 'should be' do
+    Cocaine::Worker.new
+  end
+end
