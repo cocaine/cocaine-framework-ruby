f1698a5638a82c16325102efc327535e0442161a
Evgeny Safronov <division494@gmail.com>
2013-11-09 23:14:42 +0400

    Handling invocation errors.

--- lib/cocaine/sandbox.rb
+++ lib/cocaine/sandbox.rb
@@ -3,6 +3,10 @@ $log.level = Logger::DEBUG
 
 
 class Cocaine::Sandbox
+  module ERRNO
+    NO_SUCH_EVENT = 1
+  end
+
   def initialize
     @handlers = {}
   end
@@ -12,9 +16,23 @@ class Cocaine::Sandbox
   end
 
   def invoke(event, request, response)
-    #todo: try/catch block here
+    if @handlers.has_key? event
+      invoke_handler event, request, response
+    else
+      write_error ERRNO::NO_SUCH_EVENT, "application has no event '#{event}'", response
+    end
+  end
+
+  :private
+  def invoke_handler(event, request, response)
     $log.debug "invoking '#{event}'"
     handler = @handlers[event]
     handler.execute(request, response)
+  end
+
+  :private
+  def write_error(errno, reason, response)
+    response.error errno, reason
+    response.close
   end
 end
