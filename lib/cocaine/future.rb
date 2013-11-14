47391a725cddd8bc6cb776f27723ee8f750863d9
Evgeny Safronov <division494@gmail.com>
2013-11-15 00:29:10 +0400

    Future review.

--- lib/cocaine/future.rb
+++ lib/cocaine/future.rb
@@ -1,4 +1,17 @@
 class Cocaine::Future
+  def set_value(value)
+    @value = value
+  end
+
+  def set_error(error)
+    @error = error
+  end
+
+  def get
+    raise @error if @error
+    @value
+  end
+
   def self.value(value)
     f = Cocaine::Future.new
     f.set_value value
@@ -9,18 +22,5 @@ class Cocaine::Future
     f = Cocaine::Future.new
     f.set_error error
     f
-  end
-
-  def get
-    raise @error if @error
-    @value
-  end
-
-  def set_value(value)
-    @value = value
-  end
-
-  def set_error(error)
-    @error = error
   end
 end
