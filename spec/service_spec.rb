eca484588e1c6c1c7da705bd1606e86e494c444d
Evgeny Safronov <division494@gmail.com>
2013-11-19 15:45:46 +0400

    Synchrony single chunk test.

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -108,85 +108,19 @@ describe Cocaine::Synchrony::Service do
     expect(flag).to be true
   end
 
-  #example 'synchrony usage of echo service' do
-  #  EM.synchrony do
-  #    service = Cocaine::Synchrony::Service.new 'echo-ruby'
-  #    ch = service.enqueue('ping', 'message')
-  #    msg = ch.read
-  #    expect(msg).to eq('message')
-  #    EM.stop
-  #  end
-  #end
-  #
-  #example 'synchrony usage of streaming echo service' do
-  #  EM.synchrony do
-  #    service = Cocaine::Synchrony::Service.new 'echo-ruby'
-  #    ch = service.enqueue('ping-streaming', 'message')
-  #    msg = [nil] * 3
-  #    msg[0] = ch.read
-  #    msg[1] = ch.read
-  #    msg[2] = ch.read
-  #    expect(msg).to eq(['message', 'message!', 'message! :)'])
-  #    EM.stop
-  #  end
-  #end
-  #
-  #example 'synchrony usage of streaming echo service with partial collect method' do
-  #  EM.synchrony do
-  #    service = Cocaine::Synchrony::Service.new 'echo-ruby'
-  #    ch = service.enqueue('ping-streaming', 'message')
-  #    msg= ch.collect(2)
-  #    expect(msg).to eq(['message', 'message!'])
-  #    EM.stop
-  #  end
-  #end
-  #
-  #example 'synchrony usage of streaming echo service with full collect method' do
-  #  EM.synchrony do
-  #    service = Cocaine::Synchrony::Service.new 'echo-ruby'
-  #    ch = service.enqueue('ping-streaming', 'message')
-  #    msg= ch.collect(3)
-  #    expect(msg).to eq(['message', 'message!', 'message! :)'])
-  #    EM.stop
-  #  end
-  #end
-  #
-  #example 'synchrony usage of streaming echo service with collect until choke method' do
-  #  EM.synchrony do
-  #    service = Cocaine::Synchrony::Service.new 'echo-ruby'
-  #    ch = service.enqueue('ping-streaming', 'message')
-  #    msg= ch.collect()
-  #    expect(msg).to eq(['message', 'message!', 'message! :)'])
-  #    EM.stop
-  #  end
-  #end
-  #
-  #example 'synchrony usage of streaming echo service with each' do
-  #  EM.synchrony do
-  #    service = Cocaine::Synchrony::Service.new 'echo-ruby'
-  #    ch = service.enqueue('ping-streaming', 'message')
-  #    results = []
-  #    ch.each do |result|
-  #      results.push result
-  #    end
-  #    expect(results).to eq(['message', 'message!', 'message! :)'])
-  #    EM.stop
-  #  end
-  #end
-  #
-  #example 'synchrony usage of service with wrong event name' do
-  #  EM.synchrony do
-  #    service = Cocaine::Synchrony::Service.new 'echo-ruby'
-  #    ch = service.enqueue('ping-wrong', 'message')
-  #    expect { ch.read }.to raise_error(Cocaine::ServiceError)
-  #    EM.stop
-  #  end
-  #end
-  #
-  #example 'synchrony throws exception when service is not available' do
-  #  EM.synchrony do
-  #    expect { Cocaine::Synchrony::Service.new 'non-existing-app' }.to raise_error(Cocaine::ServiceError)
-  #    EM.stop
-  #  end
-  #end
+  it 'should synchrony read exactly one chunk' do
+    EM.synchrony do
+      expected = {endpoint: ['127.0.0.1', 10054], version: 1, api: {0 => 'enqueue', 1 => 'info'}}
+      server = CocaineRuntimeMock.new
+      server.register 'mock-app', 1, expected
+      server.when('mock-app').message([0, 1, ['ping', 'message']]) { ['chunk#1'] }
+      server.run
+
+      service = Cocaine::Synchrony::Service.new 'mock-app'
+      ch = service.enqueue('ping', 'message')
+      msg = ch.read
+      expect(msg).to eq('chunk#1')
+      EM.stop
+    end
+  end
 end
